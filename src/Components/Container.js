fe059ddb319d9138adac240579685aa94327085b
Dani Nanestean <dnanestean@gmail.com>
2018-08-24 15:21:44 +0300

    - added the container

---
+++ src/Components/Container.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const StyledContainer = styled.section`
+  max-width: 960px
+  margin: 0 auto
+`
+
+const Container = () => (
+
+  <StyledContainer>
+    <h1>HTDB Audio</h1>
+  </StyledContainer>
+
+);
+
+
+export default Container
